85ac5256d72743ba87a5c2c23462b0d2ffc59e57
Игорь Павленко <fr0m.smef@gmail.com>
2026-01-14 18:41:20 +0300

    Update VersionBadge.tsx

--- src/components/VersionBadge.tsx
+++ src/components/VersionBadge.tsx
@@ -147,7 +147,7 @@ export function VersionBadge() {
                       Спонсорство
                     </a>
                     <a
-                      href="https://t.me/ifsmef"
+                      href="https://t.me/+YvNidsv9cwQ2MGYy"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg flex items-center gap-2 text-sm transition-colors"
